7fde975fbc36c527420a0d764be2fbfe274fe833
frankvega2010 <franco_vega@msn.com>
2020-02-16 23:39:31 -0300

    Added basic player controls, WIP Staff of Lighting

---
+++ Dungeon Crawler/Assets/Scripts/Player/Weapons/LightingBeam.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightingBeam : MonoBehaviour
+{
+    [Header("General Settings")]
+    public float fireRate;
+    public float damage;
+
+    [Header("Checking Variables")]
+    public bool isActive;
+    public bool canDamage;
+    public bool isTouching;
+    public List<GameObject> enemiesTouching;
+    public float fireRateTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canDamage = true;
+        isActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isActive)
+        {
+            fireRateTimer += Time.deltaTime;
+
+            if (fireRateTimer >= fireRate)
+            {
+                fireRateTimer = 0;
+                //canDamage = true;
+            }
+
+            /*if (canDamage)
+            {
+                canDamage = false;
+            }*/
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "enemy")
+        {
+            Debug.Log("Damage Given: " + damage);
+            isTouching = true;
+            enemiesTouching.Add(other.gameObject);
+
+            /*if (canDamage)
+            {
+                
+            }*/
+        }
+    }
+}
---
+++ Dungeon Crawler/Assets/Scripts/Player/Weapons/MageStaff.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MageStaff : MonoBehaviour
+{
+    [Header("General Settings")]
+    //public KeyCode attackButton;
+    public int mouseButton;
+    public float damage;
+    public float fireRate;
+
+    [Header("Components Assigment")]
+    public GameObject lightingGameObject;
+    public GameObject model;
+    private Animator staffAnimator;
+    private LightingBeam lightingBeam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lightingBeam = lightingGameObject.GetComponent<LightingBeam>();
+        lightingBeam.damage = damage;
+        lightingBeam.fireRate = fireRate;
+        lightingGameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(mouseButton))
+        {
+            lightingGameObject.SetActive(true);
+            lightingBeam.isActive = true;
+            // Begin Animation
+        }
+
+        if (Input.GetMouseButtonUp(mouseButton))
+        {
+            lightingBeam.isActive = false;
+            lightingBeam.canDamage = false;
+            // End Animation
+            lightingGameObject.SetActive(false);
+        }
+    }
+}
